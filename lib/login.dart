862b740c9468771994def29e048ac2640817ebe2
Denis Viana <denisvcosta@gmail.com>
2019-05-08 00:23:09 -0300

    Fix gradle

--- lib/login.dart
+++ lib/login.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:my_chat_app/const.dart';
 import 'package:my_chat_app/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:my_chat_app/chat.dart';
 import 'package:my_chat_app/const.dart';
 import 'package:my_chat_app/login.dart';
